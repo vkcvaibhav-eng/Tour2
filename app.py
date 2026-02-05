c473dea71007da2a74df93424967a53fcb44cc0f
vkcvaibhav-eng <vkcvaibhav@gmail.com>
2026-02-05 20:34:46 +0530

    Update app.py

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ from fpdf import FPDF
 from datetime import datetime
 
 # --- CONFIGURATION ---
-AI_MODEL_NAME = "gemini-3-pro-preview" 
+AI_MODEL_NAME = "gemini-3-flash-preview" 
 ST_DATA_DIR = "data_store"
 ST_RULES_DIR = "rules_store"
 os.makedirs(ST_DATA_DIR, exist_ok=True)
@@ -428,4 +428,5 @@ with tabs[2]:
 
         pdf_bytes = pdf.output(dest='S').encode('latin-1', 'ignore')
         st.download_button("Download PDF", pdf_bytes, "NAU_Tour_Diary.pdf", "application/pdf")
+
 
